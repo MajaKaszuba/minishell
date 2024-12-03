a27d9d24f8cc5109cf19174d0ccb6a81a7502704
MajaKaszuba <87532762+MajaKaszuba@users.noreply.github.com>
2024-12-03 14:18:58 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -12,36 +12,70 @@
 
 #include "../include/minishell.h"
 
-int	are_we_rich(char **tokens)
+char	*expand_env_variables(char *token)
+{
+	char	*result;
+	char	*env_value;
+	int		i;
+	int		start;
+
+	result = ft_strdup(""); // Pusty wynik
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (token[i])
+	{
+		if (token[i] == '$' && token[i + 1] != '\0') // Znaleziono '$'
+		{
+			start = i + 1;
+			while (token[i + 1] && (ft_isalnum(token[i + 1]) || token[i + 1] == '_'))
+				i++;
+			env_value = get_env_value(token, start, i);
+			if (!env_value) // Obsługa błędów
+			{
+				free(result);
+				return (NULL);
+			}
+			result = append_env_value(result, env_value);
+			free(env_value);
+		}
+		else
+			result = append_char(result, token[i]); // Dodaj bieżący znak
+		i++;
+	}
+	return (result);
+}
+
+void	are_we_rich(char **tokens)
 {
 	int		i;
-	char	*env_value;
+	char	*expanded;
 
 	i = 0;
 	while (tokens[i])
 	{
-		if (tokens[i][0] == '$')
+		if (ft_strchr(tokens[i], '$')) // Sprawdź, czy token zawiera '$'
 		{
-			env_value = getenv(tokens[i] + 1);
-			if (env_value)
-			{
-				free(tokens[i]);
-				tokens[i] = ft_strdup(env_value);
-			}
+			expanded = expand_env_variables(tokens[i]);
+			free(tokens[i]);
+			tokens[i] = expanded;
 		}
 		i++;
 	}
-	return (0);
 }
 
 int	find_closing_quote(char **tokens, int start, char quote_type)
 {
 	int	i;
 
-	i = start + 1;
+	i = start;
 	while (tokens[i])
 	{
-		if (tokens[i][ft_strlen(tokens[i]) - 1] == quote_type)
+		// Jeśli cudzysłów zamykający jest w tym samym tokenie
+		if (i == start && tokens[i][ft_strlen(tokens[i]) - 1] == quote_type)
+			return (i);
+		// Jeśli cudzysłów zamykający jest w innym tokenie
+		else if (i != start && tokens[i][ft_strlen(tokens[i]) - 1] == quote_type)
 			return (i);
 		i++;
 	}
@@ -94,23 +128,67 @@ void	single_bunny(char **tokens)
 	int		i;
 	int		closing_quote;
 	char	*merged;
-	char	quote_type;
 
 	i = 0;
 	while (tokens[i])
 	{
-		if (tokens[i][0] == '\'') // Znajdź otwierający cudzysłów
+		// Znajdź otwierający pojedynczy cudzysłów
+		if (tokens[i][0] == '\'')
 		{
-			quote_type = tokens[i][0];
-			closing_quote = find_closing_quote(tokens, i, quote_type);
+			closing_quote = find_closing_quote(tokens, i, '\'');
 			if (closing_quote == -1) // Brak zamykającego cudzysłowu
 			{
 				printf("Error: unmatched single quote in input.\n");
 				return ;
 			}
-			merged = merge_tokens(tokens, i, closing_quote, quote_type);
+			// Połącz tokeny w jeden ciąg
+			merged = merge_tokens(tokens, i, closing_quote, '\'');
 			free(tokens[i]);
 			tokens[i] = merged;
+			// Usuń przetworzone tokeny
+			remove_merged_tokens(tokens, i, closing_quote);
+		}
+		else
+			i++;
+	}
+}
+
+void	double_bunny(char **tokens)
+{
+	int		i;
+	int		closing_quote;
+	char	*merged;
+
+	i = 0;
+	while (tokens[i])
+	{
+		if (tokens[i][0] == '"') // Znalezienie otwierającego cudzysłowu
+		{
+			// Sprawdź, czy zamykający cudzysłów jest w tym samym tokenie
+			if (tokens[i][ft_strlen(tokens[i]) - 1] == '"')
+			{
+				merged = ft_strdup(tokens[i] + 1);
+				merged[ft_strlen(merged) - 1] = '\0';
+				free(tokens[i]);
+				tokens[i] = merged;
+				are_we_rich(tokens + i); // Zamień zmienne środowiskowe
+				i++;
+				continue;
+			}
+			// Szukanie zamykającego cudzysłowu w innych tokenach
+			closing_quote = find_closing_quote(tokens, i, '"');
+			if (closing_quote == -1) // Błąd: brak zamykającego cudzysłowu
+			{
+				printf("Error: unmatched double quote in input.\n");
+				return ;
+			}
+			// Łączenie tokenów w jeden ciąg
+			merged = merge_tokens(tokens, i, closing_quote, '"');
+			free(tokens[i]);
+			tokens[i] = merged;
+			// Zamień zmienne środowiskowe w wynikowym ciągu
+			are_we_rich(tokens + i);
+			// Usunięcie połączonych tokenów
 			remove_merged_tokens(tokens, i, closing_quote);
 		}
 		else
